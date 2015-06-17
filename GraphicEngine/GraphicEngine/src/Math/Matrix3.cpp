776256a3e05cf0b1b9b5e0d1a0e43676f6afb733
AlexandreRivet <alex-rivet94@hotmail.fr>
2015-06-17 19:38:06 +0200

    Matrix3

--- GraphicEngine/GraphicEngine/src/Math/Matrix3.cpp
+++ GraphicEngine/GraphicEngine/src/Math/Matrix3.cpp
@@ -1,1 +1,30 @@
 #include "Math/Matrix3.h"
+
+Matrix3::Matrix3(const float* data = nullptr)
+{
+	if (data == nullptr)
+	{
+		*this = identity();
+	}
+	else
+	{
+		memcpy(m_data, data, 9 * sizeof(float));
+	}
+}
+
+Matrix3::Matrix3(const Matrix3& other)
+{
+	memcpy(m_data, other.m_data, 9 * sizeof(float));
+}
+
+Matrix3::~Matrix3()
+{
+
+}
+
+float Matrix3::getValue(uint iRow, uint iCol) const
+{
+	assert(iRow <3 && iCol < 3);
+
+	return m_data[iRow * 3 + iCol];
+}
