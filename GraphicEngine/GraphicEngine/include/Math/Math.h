e81de25f7000fe5573a1578ff41ac8c0d35a4b29
ArthurTorrente <torrente.arthur@gmail.com>
2015-06-18 22:33:39 +0200

    math, vector2

--- GraphicEngine/GraphicEngine/include/Math/Math.h
+++ GraphicEngine/GraphicEngine/include/Math/Math.h
@@ -1,8 +1,12 @@
 #ifndef _MATH_H_
 #define _MATH_H_
 
+#include <cmath>
+
 namespace Math
 {
+    extern const float TWO_PI;
+
     bool FloatEqual(float a, float b, float tolerance)
     {
         if (fabs(b - a) <= tolerance)
@@ -10,6 +14,13 @@ namespace Math
         else
             return false;
     }
+
+    static inline bool isNaN(float f)
+    {
+        return f != f;
+    }
+
+    float UnitRandom();
 }
 
 #endif //_MATH_H_
---
+++ GraphicEngine/GraphicEngine/src/Math/Math.cpp
@@ -0,0 +1,11 @@
+#include "Math/Math.h"
+
+namespace Math
+{
+    static const float TWO_PI = M_PI * 2.0f;
+
+    float UnitRandom()
+    {
+        static unsigned __int64 q = time(NULL);
+    }
+}
